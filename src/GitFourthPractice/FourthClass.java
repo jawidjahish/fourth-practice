bcc5110ee6850a06920280e93ba7df69544359bc
Jawid Jahish <jawidjahish@Jawids-MBP.attlocal.net>
2022-06-22 14:17:10 -0700

    forth code

---
+++ src/GitFourthPractice/FourthClass.java
@@ -0,0 +1,24 @@
+package GitFourthPractice;
+
+import java.util.HashMap;
+
+public class FourthClass {
+
+	public static void main(String[] args) {
+ 
+		
+		
+		HashMap<Integer,String> map = new HashMap<>();
+		map.put(1, "name1");
+		map.put(2, "name2");
+		map.put(3, "name3");
+		map.put(4, "name4");
+		map.put(5, "name5");
+		map.put(6, "name6");
+		map.put(7, "name7");
+		System.out.println(map);
+		
+		
+	}
+
+}
